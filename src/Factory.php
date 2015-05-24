c6c5a6a364c609c7e5caa376d4d124bc6d7a4ba1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-05-25 00:17:34 +0200

    Passing on the missing options from the factory into the messenger

--- src/Factory.php
+++ src/Factory.php
@@ -11,11 +11,11 @@ class Factory
     const INTERVAL = 0.1;
     const TIMEOUT = 13;
 
-    public static function parent(Process $process, LoopInterface $loop, $interval = self::INTERVAL, $timeout = self::TIMEOUT)
+    public static function parent(Process $process, LoopInterface $loop, $interval = self::INTERVAL, array $options = [])
     {
         $process->start($loop, $interval);
 
-        return \WyriHaximus\React\tickingPromise($loop, $interval, [$process, 'isRunning'])->then(function () use ($process) {
+        return \WyriHaximus\React\tickingPromise($loop, $interval, [$process, 'isRunning'])->then(function () use ($process, $options) {
             $messenger = new Messenger($process->stdin, $process->stdout, $process->stderr, [
                 'read_err' => 'stderr',
                 'read' => 'stdout',
@@ -24,18 +24,18 @@ class Factory
                 'callForward' => function ($name, $arguments) use ($process) {
                     return call_user_func_array([$process, $name], $arguments);
                 },
-            ]);
+            ] + $options);
             return \React\Promise\resolve($messenger);
         });
     }
 
-    public static function child(LoopInterface $loop)
+    public static function child(LoopInterface $loop, array $options = [])
     {
         return new Messenger(new Stream(STDIN, $loop), new Stream(STDOUT, $loop), new Stream(STDERR, $loop), [
             'read_err' => 'stdin',
             'read' => 'stdin',
             'write_err' => 'stderr',
             'write' => 'stdout',
-        ]);
+        ] + $options);
     }
 }
