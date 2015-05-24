b4323d849d29bde0a11b0ab567897c80864cc13f
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-05-25 00:21:48 +0200

    Secure message example

---
+++ examples/secure-messages/ping.php
@@ -0,0 +1,46 @@
+<?php
+
+require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+
+use React\ChildProcess\Process;
+use React\EventLoop\Factory as EventLoopFactory;
+use React\EventLoop\Timer\Timer;
+use WyriHaximus\React\ChildProcess\Messenger\Factory as MessengerFactory;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Factory as MessagesFactory;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
+use WyriHaximus\React\ChildProcess\Messenger\Messenger;
+
+$options = [
+    'lineCLass' => 'WyriHaximus\React\ChildProcess\Messenger\Messages\SecureLine',
+    'lineOptions' => [
+        'key' => 'abc123',
+    ],
+];
+
+$loop = EventLoopFactory::create();
+
+$process = new Process('exec php ' . dirname(dirname(__DIR__)) . '/examples/messages/pong.php 2>1');
+
+MessengerFactory::parent($process, $loop, $options)->then(function (Messenger $messenger) use ($loop) {
+    $messenger->on('message', function (Payload $payload) {
+        echo $payload['time'], PHP_EOL;
+    });
+
+    $i = 0;
+    $loop->addPeriodicTimer(1, function (Timer $timer) use (&$i, $messenger) {
+        if ($i >= 13) {
+            $timer->cancel();
+            $messenger->terminate();
+            return;
+        }
+
+        $messenger->message(MessagesFactory::message([
+            'i' => $i,
+            'time' => time(),
+        ]));
+
+        $i++;
+    });
+});
+
+$loop->run();
---
+++ examples/secure-messages/pong.php
@@ -0,0 +1,30 @@
+<?php
+
+require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+
+use React\EventLoop\Factory as EventLoopFactory;
+use WyriHaximus\React\ChildProcess\Messenger\Factory as MessengerFactory;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Factory as MessagesFactory;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
+use WyriHaximus\React\ChildProcess\Messenger\Messenger;
+use WyriHaximus\React\ChildProcess\Messenger\Recipient;
+
+
+$options = [
+    'lineCLass' => 'WyriHaximus\React\ChildProcess\Messenger\Messages\SecureLine',
+    'lineOptions' => [
+        'key' => 'abc123',
+    ],
+];
+
+
+$loop = EventLoopFactory::create();
+
+$recipient = MessengerFactory::child($loop, $options);
+$recipient->on('message', function (Payload $payload, Messenger $messenger) {
+    $messenger->message(MessagesFactory::message([
+        'time' => (new DateTime('@' . $payload['time'] * $payload['i']))->format('c')
+    ]));
+});
+
+$loop->run();
