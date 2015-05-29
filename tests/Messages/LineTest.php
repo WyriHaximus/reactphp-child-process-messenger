f7a82a9c0ca6b3bdbfe4260c159b1280d7297fb9
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-05-29 23:07:20 +0200

    Fixed line test

--- tests/Messages/LineTest.php
+++ tests/Messages/LineTest.php
@@ -2,48 +2,45 @@
 
 namespace WyriHaximus\React\Tests\ChildProcess\Messenger\Messages;
 
+use WyriHaximus\React\ChildProcess\Messenger\Messages\ActionableMessageInterface;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Line;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\LineInterface;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Rpc;
 
 class LineTest extends \PHPUnit_Framework_TestCase
 {
     public function providerBasic()
     {
         yield [
-            [],
-            '[]' . PHP_EOL,
-        ];
-
-        yield [
-            [
-                'type' => 'rpc',
-                'uniqid' => 1234567890,
-                'target' => 'foo',
-                'payload' => [
+            new Rpc(
+                'foo',
+                new Payload([
                     'bar' => 'baz'
-                ],
-            ],
-            '{"type":"rpc","uniqid":1234567890,"target":"foo","payload":{"bar":"baz"}}' . PHP_EOL,
+                ]),
+                1234567890
+            ),
+            '{"type":"rpc","uniqid":1234567890,"target":"foo","payload":{"bar":"baz"}}' . LineInterface::EOL,
         ];
 
         yield [
-            [
-                'type' => 'rpc',
-                'uniqid' => 1234567890,
-                'target' => 'foo',
-                'payload' => [
+            new Rpc(
+                'foo',
+                new Payload([
                     'bar',
                     'baz',
-                ],
-            ],
-            '{"type":"rpc","uniqid":1234567890,"target":"foo","payload":["bar","baz"]}' . PHP_EOL,
+                ]),
+                1234567890
+            ),
+            '{"type":"rpc","uniqid":1234567890,"target":"foo","payload":["bar","baz"]}' . LineInterface::EOL,
         ];
     }
 
     /**
      * @dataProvider providerBasic
      */
-    public function testBasic(array $input, $output)
+    public function testBasic(\JsonSerializable $input, $output)
     {
-        $this->assertEquals($output, (string)new Line($input));
+        $this->assertEquals($output, (string)new Line($input, []));
     }
 }
