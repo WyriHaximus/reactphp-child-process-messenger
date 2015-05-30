2d7dbab9d72d7f9c637920358de8e04930bfc219
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-05-30 23:06:50 +0200

    Updated payload test

--- tests/Messages/PayloadTest.php
+++ tests/Messages/PayloadTest.php
@@ -9,7 +9,24 @@ class PayloadTest extends \PHPUnit_Framework_TestCase
 {
     public function testBasic()
     {
-        $payload = new Payload([]);
-        $this->assertEquals([], $payload->getPayload());
+        $payload = new Payload([
+            'foo' => 'bar',
+        ]);
+
+        $this->assertEquals([
+            'foo' => 'bar',
+        ], $payload->getPayload());
+        $this->assertTrue(isset($payload['foo']));
+        $this->assertEquals('bar', $payload['foo']);
+        $payload['ajsdhjkfad'] = 'abc';
+        $this->assertEquals('abc', $payload['ajsdhjkfad']);
+        unset($payload['ajsdhjkfad']);
+        $this->assertFalse(isset($payload['ajsdhjkfad']));
+        $payload[] = 'abc';
+        $this->assertEquals('abc', $payload[0]);
+        $this->assertEquals([
+            'foo' => 'bar',
+            0 => 'abc',
+        ], $payload->getPayload());
     }
 }
