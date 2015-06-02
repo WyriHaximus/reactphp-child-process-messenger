2208180393076bc0be74ff35ce9268da1077c633
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-06-02 08:02:23 +0200

    RPC Message types VO tests

---
+++ tests/Messages/RpcErrorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WyriHaximus\React\Tests\ChildProcess\Messenger\Messages;
+
+use Phake;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\RpcError;
+
+class RpcErrorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBasic()
+    {
+        $payload = new Payload([
+            'foo' => 'bar',
+        ]);
+        $message = new RpcError('abc', $payload);
+
+        $this->assertSame($payload, $message->getPayload());
+
+        $this->assertEquals('{"type":"rpc_error","uniqid":"abc","payload":{"foo":"bar"}}', json_encode($message));
+
+        $deferred = Phake::mock('React\Promise\Deferred');
+        $outstandingCall = Phake::mock('WyriHaximus\React\ChildProcess\Messenger\Messenger');
+        Phake::when($outstandingCall)->getDeferred()->thenReturn($deferred);
+        $messenger = Phake::mock('WyriHaximus\React\ChildProcess\Messenger\Messenger');
+        Phake::when($messenger)->getOutstandingCall('abc')->thenReturn($outstandingCall);
+
+        $message->handle($messenger, '');
+
+        Phake::inOrder(
+            Phake::verify($messenger)->getOutstandingCall('abc'),
+            Phake::verify($outstandingCall)->getDeferred(),
+            Phake::verify($deferred)->reject($payload)
+        );
+
+    }
+}
---
+++ tests/Messages/RpcNotifyTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WyriHaximus\React\Tests\ChildProcess\Messenger\Messages;
+
+use Phake;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\RpcNotify;
+
+class RpcNotifyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBasic()
+    {
+        $payload = new Payload([
+            'foo' => 'bar',
+        ]);
+        $message = new RpcNotify('abc', $payload);
+
+        $this->assertSame($payload, $message->getPayload());
+
+        $this->assertEquals('{"type":"rpc_notify","uniqid":"abc","payload":{"foo":"bar"}}', json_encode($message));
+
+        $deferred = Phake::mock('React\Promise\Deferred');
+        $outstandingCall = Phake::mock('WyriHaximus\React\ChildProcess\Messenger\Messenger');
+        Phake::when($outstandingCall)->getDeferred()->thenReturn($deferred);
+        $messenger = Phake::mock('WyriHaximus\React\ChildProcess\Messenger\Messenger');
+        Phake::when($messenger)->getOutstandingCall('abc')->thenReturn($outstandingCall);
+
+        $message->handle($messenger, '');
+
+        Phake::inOrder(
+            Phake::verify($messenger)->getOutstandingCall('abc'),
+            Phake::verify($outstandingCall)->getDeferred(),
+            Phake::verify($deferred)->notify($payload)
+        );
+
+    }
+}
