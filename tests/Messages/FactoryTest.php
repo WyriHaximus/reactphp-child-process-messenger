d0f090e64028a32677c8dcf57b8d76ea517daf12
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-06-04 08:06:00 +0200

    Messages factory tests

--- tests/Messages/FactoryTest.php
+++ tests/Messages/FactoryTest.php
@@ -2,11 +2,16 @@
 
 namespace WyriHaximus\React\Tests\ChildProcess\Messenger\Messages;
 
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Error;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Factory;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Line;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\LineInterface;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Message;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Rpc;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\RpcError;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\RpcNotify;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\RpcSuccess;
 
 class FactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -21,6 +26,83 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
                     'foo',
                     'bar',
                 ], $message->getPayload()->getPayload());
+                return true;
+            },
+        ];
+        yield [
+            '{"type":"error","payload":["foo","bar"]}' . LineInterface::EOL,
+            function ($message) {
+                $this->assertInstanceOf(Error::class, $message);
+                $this->assertInstanceOf(Payload::class, $message->getPayload());
+                $this->assertSame([
+                    'foo',
+                    'bar',
+                ], $message->getPayload()->getPayload());
+                return true;
+            },
+        ];
+        yield [
+            '{"type":"rpc","uniqid":"abc","target":"foo","payload":["foo","bar"]}' . LineInterface::EOL,
+            function ($message) {
+                $this->assertInstanceOf(Rpc::class, $message);
+                $this->assertInstanceOf(Payload::class, $message->getPayload());
+                $this->assertEquals([
+                    'type' => 'rpc',
+                    'uniqid' => 'abc',
+                    'target' => 'foo',
+                    'payload' => new Payload([
+                        'foo',
+                        'bar',
+                    ]),
+                ], $message->jsonSerialize());
+                return true;
+            },
+        ];
+        yield [
+            '{"type":"rpc_error","uniqid":"abc","payload":["foo","bar"]}' . LineInterface::EOL,
+            function ($message) {
+                $this->assertInstanceOf(RpcError::class, $message);
+                $this->assertInstanceOf(Payload::class, $message->getPayload());
+                $this->assertEquals([
+                    'type' => 'rpc_error',
+                    'uniqid' => 'abc',
+                    'payload' => new Payload([
+                        'foo',
+                        'bar',
+                    ]),
+                ], $message->jsonSerialize());
+                return true;
+            },
+        ];
+        yield [
+            '{"type":"rpc_success","uniqid":"abc","payload":["foo","bar"]}' . LineInterface::EOL,
+            function ($message) {
+                $this->assertInstanceOf(RpcSuccess::class, $message);
+                $this->assertInstanceOf(Payload::class, $message->getPayload());
+                $this->assertEquals([
+                    'type' => 'rpc_success',
+                    'uniqid' => 'abc',
+                    'payload' => new Payload([
+                        'foo',
+                        'bar',
+                    ]),
+                ], $message->jsonSerialize());
+                return true;
+            },
+        ];
+        yield [
+            '{"type":"rpc_notify","uniqid":"abc","payload":["foo","bar"]}' . LineInterface::EOL,
+            function ($message) {
+                $this->assertInstanceOf(RpcNotify::class, $message);
+                $this->assertInstanceOf(Payload::class, $message->getPayload());
+                $this->assertEquals([
+                    'type' => 'rpc_notify',
+                    'uniqid' => 'abc',
+                    'payload' => new Payload([
+                        'foo',
+                        'bar',
+                    ]),
+                ], $message->jsonSerialize());
                 return true;
             },
         ];
