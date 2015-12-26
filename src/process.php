1ee9e00c4207265c5ec671abc3bc13e2864af2ab
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-12-26 22:37:10 +0100

    Check multiple paths for composer autoloadere

--- src/process.php
+++ src/process.php
@@ -4,7 +4,15 @@ use React\EventLoop\Factory as LoopFactory;
 use WyriHaximus\React\ChildProcess\Messenger\Factory as MessengerFactory;
 use WyriHaximus\React\ChildProcess\Messenger\Process;
 
-require __DIR__ . '/../vendor/autoload.php';
+foreach ([
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+] as $file) {
+    if (file_exists($file)) {
+        require $file;
+        break;
+    }
+}
 
 $loop = LoopFactory::create();
 $messenger = MessengerFactory::child($loop);
