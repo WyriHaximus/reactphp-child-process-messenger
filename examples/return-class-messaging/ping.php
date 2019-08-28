51bb1da27c2b5e34385febc16486dd0336f47d4d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-08-28 08:15:38 +0200

    Messaging demo using the return child class

---
+++ examples/return-class-messaging/ping.php
@@ -0,0 +1,41 @@
+<?php
+
+require \dirname(\dirname(__DIR__)) . '/vendor/autoload.php';
+
+use React\EventLoop\Factory as EventLoopFactory;
+use React\EventLoop\TimerInterface;
+use WyriHaximus\React\ChildProcess\Messenger\Factory as MessengerFactory;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Factory as MessagesFactory;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
+use WyriHaximus\React\ChildProcess\Messenger\Messenger;
+
+$loop = EventLoopFactory::create();
+
+MessengerFactory::parentFromClass('WyriHaximus\React\ChildProcess\Messenger\ReturnChild', $loop)->done(function (Messenger $messenger) use ($loop) {
+    $messenger->on('message', function ($message) {
+        echo 'Message: ', \var_export($message, true), PHP_EOL;
+    });
+    $messenger->on('error', function ($e) {
+        echo 'Error: ', \var_export($e, true), PHP_EOL;
+    });
+
+    $i = 0;
+    $loop->addPeriodicTimer(1, function (TimerInterface $timer) use (&$i, $messenger, $loop) {
+        echo 'tick', PHP_EOL;
+        if ($i >= 13) {
+            $loop->cancelTimer($timer);
+            $messenger->softTerminate();
+
+            return;
+        }
+
+        $messenger->message(MessagesFactory::message([
+            'i' => $i,
+            'time' => \time(),
+        ]));
+
+        $i++;
+    });
+});
+
+$loop->run();
